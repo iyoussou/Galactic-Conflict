fd49cb1204b551e9a1efeacd6ed7e12a9ba50b9e
Ismail Youssou <iyoussou@uchicago.edu>
2021-04-17 20:43:04 -0500

    added function build_deck ver 1.0: reads deck and makes cards that are an attack of a desired value, or -1

--- cpp/game.cpp
+++ cpp/game.cpp
@@ -8,7 +8,14 @@
 #include "color.h"
 using namespace std;
 
-enum CardType { attack, negotiate, artifact, flare };
+struct card {
+    int attack = -1;
+    bool negotiate;
+    bool artifact;
+    bool flare;
+    string name;
+};
+typedef struct card card;
 
 struct player {
     string alien;
@@ -18,8 +25,31 @@ struct player {
     planet p4;
     planet p5;
 };
-
 typedef struct player player;
+
+//formats cards in deck.txt and constructs a deck
+vector<card> build_deck()
+{
+    string cur;
+    vector<card> deck;
+    ifstream decklist("deck.txt");
+    while(getline(decklist, cur))
+    {
+        if(cur[0] == 'a') //if attack
+        {
+            card a;
+            a.attack = (int) stoi(cur.substr(2,cur.length()));
+            deck.push_back(a);
+        }
+        else //if not an attack (will need to be expanded)
+        {
+            card r;
+            r.name = cur;
+            deck.push_back(r);
+        }
+    }
+    return deck;
+}
 
 int game()  {
     // initialize game
@@ -28,6 +58,11 @@ int game()  {
     cout << Ismail.p1.habitants.size() << "\n";
     Ismail.p1 = remove_ships(Ismail.p1, green, 6);
     cout << Ismail.p1.habitants.size() << "\n";
+    vector<card> deck = build_deck();
+    for(unsigned int i = 0; i < deck.size(); i++)
+    {
+        cout << deck.at(i).attack << "\n";
+    }
     // start turn
     // regroup phase
     // destiny
